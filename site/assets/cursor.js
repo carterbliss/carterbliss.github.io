20ea8e9759b221c41d1d64dd8b2525adc56e6e45
Carter Bliss <cobbliss@gmail.com>
2026-04-07 15:50:50 -0400

    Reduce pointer cursor size to 22x30px

--- site/assets/cursor.js
+++ site/assets/cursor.js
@@ -71,7 +71,7 @@
 
   const handImg = new Image();
   handImg.onload = function () {
-    const W = 27, H = 35;
+    const W = 22, H = 30;
     const offscreen = document.createElement('canvas');
     offscreen.width = W; offscreen.height = H;
     const cx = offscreen.getContext('2d');
